d965a3e6d19ca4c744aee18a4fceb24f39a92a65
RengbarH <86048645+RengbarH@users.noreply.github.com>
2022-05-24 14:16:39 +0200

    Verknüpfungs commit

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import PersonsView from '../views/PersonsView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: AboutView
+  },
+  {
+    path: '/persons',
+    name: 'persons',
+    component: PersonsView
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  linkActiveClass: 'active',
+  routes
+})
+
+export default router
