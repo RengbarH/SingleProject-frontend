96886029beb2537f355fd21f3f6019e878623dc6
RengbarH <86048645+RengbarH@users.noreply.github.com>
2022-06-01 13:31:58 +0200

    Dynamische View zur Ansicht von Personen

--- src/views/PersonsView.vue
+++ src/views/PersonsView.vue
@@ -1,10 +1,52 @@
 <template>
-<h1>Welcome to Persons site</h1>
+  <div class="row row-cols-1 row-cols-md-2 g-4">
+    <div class="col" v-for="person in persons" :key="person.id">
+      <div class="card">
+        <img :src="getAvatar(person)" class="card-img-top" :alt="person.firstName + ' ' + person.lastName">
+        <div class="card-body">
+          <h5 class="card-title">{{ `${person.firstName} ${person.lastName}` }}</h5>
+          <p class="card-text">
+            {{ `${person.firstName} ${person.lastName}` }} ist {{ person.gender === 'MALE' ? 'männlich' : 'weiblich' }} und besitzt
+            {{ person.recipe.length }} Rezepte
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'PersonsView'
+  name: 'PersonsView',
+  data () {
+    return {
+      persons: [
+        {
+          id: 1,
+          firstName: 'Max',
+          lastName: 'Mustermann',
+          gender: 'MALE',
+          recipe: []
+        },
+        {
+          id: 2,
+          firstName: 'Maia',
+          lastName: 'Hedwig',
+          gender: 'FEMALE',
+          recipe: []
+        }
+      ]
+    }
+  },
+  methods: {
+    getAvatar (person) {
+      if (person.gender === 'MALE') {
+        return require('../assets/man.png')
+      } else if (person.gender === 'FEMALE') {
+        return require('../assets/woman.png')
+      }
+    }
+  }
 }
 </script>
 
