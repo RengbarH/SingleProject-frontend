9edaad30bbf04325b8dc3d2d8858601e5df257e6
RengbarH <86048645+RengbarH@users.noreply.github.com>
2022-06-27 21:54:09 +0200

    Bootstrap Button Bug gefixt

--- src/components/PersonsCreateForm.vue
+++ src/components/PersonsCreateForm.vue
@@ -1,11 +1,12 @@
 <template>
   <button class="btn btn-success sticky-button" data-bs-toggle="offcanvas" data-bs-target="#persons-create-offcanvas"
           aria-controls="#persons-create-offcanvas">
-    <i class="bi bi-persons-plus-fill"></i>
+    <i class="bi bi-person-plus-fill"></i>
   </button>
   <div class="offcanvas offcanvas-end" tabindex="-1" id="persons-create-offcanvas" aria-labelledby="offcanvas-label">
     <div class="offcanvas-header">
       <h5 id="offcanvas-label">New Person</h5>
+      <i class="bi bi-person-plus-fill"></i>
       <button type="button" id="close-offcanvas" class="btn-close text-reset" data-bs-dismiss="offcanvas"
               aria-label="Close"></button>
     </div>
